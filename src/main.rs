2752d27059003881f810af1fa9e1f22fa88f5e55
Brian Hicks <brian@brianthicks.com>
2023-01-05 16:38:05 -0600

    split CompileGrammar out to a separate struct

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,27 @@ use std::path::PathBuf;
 #[derive(Debug, Parser)]
 enum Command {
     /// Compile a tree-sitter grammar to a shared library for future use
-    CompileGrammar {
-        /// The name of the language
-        name: String,
-
-        /// The path to the source
-        path: PathBuf,
-    },
+    CompileGrammar(CompileGrammar),
 }
 
 impl Command {
+    fn run(&self) -> Result<()> {
+        match self {
+            Self::CompileGrammar(cg) => cg.run()
+        }
+    }
+}
+
+#[derive(Debug, Parser)]
+struct CompileGrammar {
+    /// The name of the language
+    name: String,
+
+    /// The path to the source
+    path: PathBuf,
+}
+
+impl CompileGrammar {
     fn run(&self) -> Result<()> {
         println!("{self:#?}");
 
