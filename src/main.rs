1f3b77fe2db13ebc5ebe80ec19ee44d170848d1a
Brian Hicks <brian@brianthicks.com>
2023-01-05 16:40:22 -0600

    final errors should be printed to stderr

--- src/main.rs
+++ src/main.rs
@@ -39,7 +39,7 @@ fn main() {
     let opts = Command::parse();
 
     if let Err(err) = opts.run() {
-        println!("{err:?}");
+        eprintln!("{err:?}");
         std::process::exit(1);
     }
 }
