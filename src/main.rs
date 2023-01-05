abb8d789398f012b1b9f39d26ce773acd9b9225a
Brian Hicks <brian@brianthicks.com>
2023-01-05 15:52:16 -0600

    add color_eyre

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use clap::Parser;
+use color::eyre::eyre::Result;
 
 #[derive(Debug, Parser)]
 struct Opts {}
 
 fn main() {
+    color_eyre::install()?;
+
     let opts = Opts::parse();
 
     println!("{opts:#?}");
