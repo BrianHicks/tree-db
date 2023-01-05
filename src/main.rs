5603700ff76349edd0ef548eb80eb81a319e59a9
Brian Hicks <brian@brianthicks.com>
2023-01-05 15:54:17 -0600

    be able to return an error

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,23 @@
 use clap::Parser;
-use color::eyre::eyre::Result;
+use color_eyre::eyre::Result;
 
 #[derive(Debug, Parser)]
-struct Opts {}
+struct TreeDB {}
+
+impl TreeDB {
+    fn run(&self) -> Result<()> {
+        println!("{self:#?}");
+
+        Ok(())
+    }
+}
 
 fn main() {
-    color_eyre::install()?;
+    color_eyre::install().expect("could not install error handlers");
 
-    let opts = Opts::parse();
-
-    println!("{opts:#?}");
+    let opts = TreeDB::parse();
+    if let Err(err) = opts.run() {
+        println!("{err:?}");
+        std::process::exit(1);
+    }
 }
