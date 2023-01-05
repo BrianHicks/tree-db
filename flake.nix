f163897ab74745e6765c64dc60001bbd22f7568d
Brian Hicks <brian@brianthicks.com>
2023-01-05 16:09:00 -0600

    vendor tree-sitter-rust

--- flake.nix
+++ flake.nix
@@ -2,12 +2,25 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+
+    # grammars
+    tree-sitter-rust = {
+      url = "github:tree-sitter/tree-sitter-rust";
+      flake = false;
+    };
   };
 
   outputs = inputs:
     inputs.flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import inputs.nixpkgs { inherit system; };
-      in {
+      let
+        pkgs = import inputs.nixpkgs { inherit system; };
+        vendor-languages = pkgs.writeShellScriptBin "vendor-languages" ''
+          rm -rf vendor
+          mkdir vendor
+          ln -s ${inputs.tree-sitter-rust} vendor/tree-sitter-rust
+        '';
+      in
+      {
         formatter = pkgs.nixpkgs-fmt;
         devShell = pkgs.mkShell {
           packages = [
@@ -17,6 +30,8 @@
             pkgs.rustfmt
             pkgs.clippy
             pkgs.rust-analyzer
+
+            vendor-languages
           ];
         };
       });
