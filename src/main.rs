3783aaa12ae4236ccbf8c5d7c74ae9fd355f609d
Brian Hicks <brian@brianthicks.com>
2023-01-06 10:02:56 -0600

    it works now!

--- src/main.rs
+++ src/main.rs
@@ -33,11 +33,22 @@ struct CompileGrammar {
     #[arg(long, default_value("."))]
     out_dir: PathBuf,
 
+    /// Target system to build for
     #[arg(long, default_value(guess_host_triple::guess_host_triple()))]
     target: String,
 
+    /// Host system to build from
     #[arg(long, default_value(guess_host_triple::guess_host_triple()))]
     host: String,
+
+    /// If present, include debugging info in built library
+    #[arg(long)]
+    debug: bool,
+
+    /// Optimization level. Corresponds roughly to clang's `-O` flag
+    /// (https://clang.llvm.org/docs/CommandGuide/clang.html#cmdoption-o0)
+    #[arg(short('O'), long, default_value = "2")]
+    opt_level: u32,
 }
 
 // TODO: Windows support should be possible, but I'm not sure how to do it right now
@@ -56,7 +67,7 @@ impl CompileGrammar {
         builder
             .opt_level(2)
             .cargo_metadata(false)
-            .debug(true)
+            .debug(self.debug)
             .include(&self.path)
             .out_dir(&self.out_dir)
             .target(&self.target)
@@ -73,7 +84,6 @@ impl CompileGrammar {
                 parser_path.display(),
             );
         }
-        builder.file(parser_path);
 
         let mut scanner_path = self.path.join("scanner.c");
         if !scanner_path.exists() {
@@ -85,13 +95,40 @@ impl CompileGrammar {
                 );
             }
             builder.cpp(true);
+            tracing::info!("enabling C++ compilation");
         }
         tracing::debug!(?scanner_path, "found scanner");
-        builder.file(scanner_path);
 
-        builder
-            .try_compile(&format!("{}.{}", self.name, DYLIB_EXTENSION))
-            .wrap_err("could not compile grammar")
+        let mut command = builder
+            .try_get_compiler()
+            .wrap_err("could not get compiler")?
+            .to_command();
+
+        if cfg!(unix) {
+            //the `cc` crate will try to compile one of these files at once,
+            // but we can compile both in one command. This is necessary in
+            // situations where the source is read-only, and is more efficient
+            // anyway.
+            command
+                .arg(&parser_path)
+                .arg(&scanner_path)
+                .arg("-o")
+                .arg(format!("{}.{}", self.name, DYLIB_EXTENSION));
+
+            tracing::info!(?command, "executing");
+
+            let status = command
+                .status()
+                .wrap_err_with(|| format!("could not execute {:?}", command.get_program()))?;
+
+            match status.code() {
+                Some(0) => Ok(()),
+                Some(other) => bail!("compilation command exited with status {}", other),
+                None => bail!("command was terminated by a signal"),
+            }
+        } else {
+            bail!("grammar compilation for this platform is probably possible, but the author doesn't have a machine to test on. Get in touch!")
+        }
     }
 }
 
