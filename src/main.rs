47ae63c6f583d89b30330f3efad37a5f5a7f4536
Brian Hicks <brian@brianthicks.com>
2023-01-06 09:06:01 -0600

    slightly less wrong

--- src/main.rs
+++ src/main.rs
@@ -41,48 +41,16 @@ struct CompileGrammar {
 }
 
 // TODO: Windows support should be possible, but I'm not sure how to do it right now
-#[cfg(unix)]
+#[cfg(all(unix, not(target_os = "macos")))]
 static DYLIB_EXTENSION: &str = "so";
+
+#[cfg(target_os = "macos")]
+static DYLIB_EXTENSION: &str = "dylib";
 
 impl CompileGrammar {
     #[instrument]
     fn run(&self) -> Result<()> {
-        let temp = tempfile::TempDir::new()
-            .wrap_err("could not create temporary directory for use in compilation")?;
-
-        self.compile_scanner(&temp)
-            .wrap_err("could not compile scanner")?;
-
-        self.compile_parser(&temp)
-            .wrap_err("could not compile grammar")?;
-
-        // // For tomorrow: I think what I need to do here is compile the scanner.c (or scanner.cc) to a scanner.o and the include that in the compilation for parser.c
-        // let mut builder = cc::Build::new();
-        // builder
-        //     .opt_level(2)
-        //     .cargo_metadata(false)
-        //     .shared_flag(true)
-        //     .debug(true)
-        //     .include(&self.path)
-        //     .out_dir(&self.out_dir)
-        //     .target(&self.target)
-        //     .host(&self.target)
-        //     .flag("-fno-exceptions");
-
-        // let parser_path = self.path.join("parser.c");
-        // if !parser_path.exists() {
-        //     bail!("parser path (`{}`) does not exist", parser_path.display())
-        // }
-        // tracing::trace!(?parser_path, "calculated parser path");
-        // builder.file(parser_path);
-
-        // builder.compile(&format!("{}.{}", self.name, DYLIB_EXTENSION));
-
-        Ok(())
-    }
-
-    #[instrument]
-    fn compile_scanner(&self, out: &tempfile::TempDir) -> Result<()> {
+        // working command: clang -I vendor/tree-sitter-rust/src vendor/tree-sitter-rust/src/parser.c vendor/tree-sitter-rust/src/scanner.c -o thingy.so -shared -fpic -o2
         let mut builder = cc::Build::new();
 
         builder
@@ -90,54 +58,40 @@ impl CompileGrammar {
             .cargo_metadata(false)
             .debug(true)
             .include(&self.path)
-            .out_dir(&out)
-            .target(&self.target)
-            .host(&self.host)
-            .warnings(false)
-            .flag("-fno-exceptions");
-
-        let mut file = self.path.join("scanner.c");
-        if !file.exists() {
-            tracing::debug!("scanner.c does not exist; trying scanner.cc");
-
-            file.set_extension("cc");
-            if !file.exists() {
-                bail!("scanner does not exist at either scanner.c or scanner.cc");
-            }
-            builder.cpp(true);
-        }
-
-        tracing::info!(source = ?file, "building scanner.o");
-        builder
-            .file(&file)
-            .try_compile("scanner")
-            .wrap_err_with(|| format!("could not compile `{}`", file.display()))?;
-
-        Ok(())
-    }
-
-    #[instrument]
-    fn compile_parser(&self, temp: &tempfile::TempDir) -> Result<()> {
-        let mut builder = cc::Build::new();
-
-        builder
-            .opt_level(2)
-            .cargo_metadata(false)
-            .debug(true)
-            .include(&self.path)
-            .include(&temp)
             .out_dir(&self.out_dir)
             .target(&self.target)
             .host(&self.host)
             .warnings(false)
-            .flag("-fno-exceptions")
-            // make this a shared library
             .shared_flag(true)
             .pic(true)
-            // from this file
-            .file(self.path.join("parser.c"))
-            .try_compile(&format!("{}.dylib", self.name))
-            .wrap_err("could not compile parser.c")
+            .flag("-fno-exceptions");
+
+        let parser_path = self.path.join("parser.c");
+        if !parser_path.exists() {
+            bail!(
+                "parser (should be `{}`) does not exist",
+                parser_path.display(),
+            );
+        }
+        builder.file(parser_path);
+
+        let mut scanner_path = self.path.join("scanner.c");
+        if !scanner_path.exists() {
+            scanner_path.set_extension("cc");
+            if !scanner_path.exists() {
+                bail!(
+                    "scanner (`scanner.c` or `scanner.cc` at `{}`) does not exist",
+                    self.path.display()
+                );
+            }
+            builder.cpp(true);
+        }
+        tracing::debug!(?scanner_path, "found scanner");
+        builder.file(scanner_path);
+
+        builder
+            .try_compile(&format!("{}.{}", self.name, DYLIB_EXTENSION))
+            .wrap_err("could not compile grammar")
     }
 }
 
