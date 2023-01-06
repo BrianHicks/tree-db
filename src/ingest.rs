419d794d777ae3a1504e72ec5b2ea7eb0190728d
Brian Hicks <brian@brianthicks.com>
2023-01-06 13:22:55 -0600

    start parsing files

--- src/ingest.rs
+++ src/ingest.rs
@@ -18,14 +18,27 @@ pub struct Ingest {
         env = "TREE_DB_LANGUAGE_SEARCH_PATH"
     )]
     include: Vec<PathBuf>,
+
+    /// The files to ingest
+    file: Vec<PathBuf>,
 }
 
 impl Ingest {
     #[instrument]
     pub fn run(&self) -> Result<()> {
-        let parser = self
+        let mut parser = self
             .parser_for(&self.language)
             .wrap_err("could not find language")?;
+
+        println!("{:#?}", parser.language());
+
+        for path in &self.file {
+            let source = std::fs::read_to_string(&path)
+                .wrap_err_with(|| format!("could not read `{}`", path.display()))?;
+
+            let tree = parser.parse(source, None);
+            println!("{tree:#?}");
+        }
 
         Ok(())
     }
@@ -39,22 +52,36 @@ impl Ingest {
 
         let mut parser = Parser::new();
 
-        unsafe {
-            let lib = libloading::Library::new(&grammar_path).wrap_err_with(|| {
-                format!(
-                    "could not open shared library ({}) for grammar",
-                    grammar_path.display()
-                )
-            })?;
+        let lib = unsafe { libloading::Library::new(&grammar_path) }.wrap_err_with(|| {
+            format!(
+                "could not open shared library ({}) for grammar",
+                grammar_path.display()
+            )
+        })?;
 
-            let lang: libloading::Symbol<unsafe extern "C" fn() -> Language> = lib
+        let lang = unsafe {
+            let lang_fn: libloading::Symbol<unsafe extern "C" fn() -> Language> = lib
                 .get(symbol_name.as_bytes())
                 .wrap_err_with(|| format!("could not load language function `{}`", symbol_name))?;
 
-            parser
-                .set_language(lang())
-                .wrap_err("could not set language")?;
-        }
+            lang_fn()
+        };
+
+        // HACK: this keeps the library's memory allocated for the duration of
+        // the program. This is necessary, since we've just called `lang` to get
+        // a reference to the grammar, and if the library gets unloaded before
+        // we parse then we'll get segfaults. An alternative eventually be to
+        // keep a mapping of language name to `libloading::Library` around.
+        //
+        // The docs for `std::mem::forget` say that a reference into the memory
+        // passed to it will not always be valid, but it looks Helix does this
+        // and it works fine. Diffsitter prefers to use `Box::leak` instead.
+        // We'll see what we see, I guess.
+        std::mem::forget(lib);
+
+        parser
+            .set_language(lang)
+            .wrap_err("could not set language")?;
 
         Ok(parser)
     }
