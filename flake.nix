529f3353739d2cb39b5e289c8fd9b8874d396fc4
Brian Hicks <brian@brianthicks.com>
2023-01-12 16:38:13 -0600

    build tree-db with Nix

--- flake.nix
+++ flake.nix
@@ -3,6 +3,9 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
 
+    naersk.url = "github:nix-community/naersk";
+    naersk.inputs.nixpkgs.follows = "nixpkgs";
+
     # grammars
     tree-sitter-rust = {
       url = "github:tree-sitter/tree-sitter-rust";
@@ -14,14 +17,24 @@
     inputs.flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import inputs.nixpkgs { inherit system; };
+
         vendor-languages = pkgs.writeShellScriptBin "vendor-languages" ''
           rm -rf vendor
           mkdir vendor
           ln -s ${inputs.tree-sitter-rust} vendor/tree-sitter-rust
         '';
+
+        naersk = pkgs.callPackage inputs.naersk { };
+
+        tree-db = naersk.buildPackage {
+          src = ./.;
+        };
       in
-      {
+      rec {
         formatter = pkgs.nixpkgs-fmt;
+
+        packages.tree-db = tree-db;
+
         devShell = pkgs.mkShell {
           packages = [
             pkgs.cargo
